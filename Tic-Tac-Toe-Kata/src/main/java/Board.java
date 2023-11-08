a396e8af6526377c5e30288ab255a5ea9056ba16
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2023-11-08 16:58:00 +0100

    refactor: refactor board

--- Tic-Tac-Toe-Kata/src/main/java/Board.java
+++ Tic-Tac-Toe-Kata/src/main/java/Board.java
@@ -4,10 +4,18 @@ public class Board {
 	private Box [][] board = new Box[3][3];
 
 	public Board() {
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board.length; j++) {
+				board[i][j] = new Box();
+			}
+		}
 	}
 	public String getStatusBoxInBoard(int positionX, int positionY) {
-		return board[positionX][positionY].getState("");
+		return board[positionX][positionY].getState();
 	}
 
 
+	public void setStatusBoxInBoard(int positionX, int positionY, String state) {
+		this.board[positionX][positionY].setState(state);
+	}
 }
--- Tic-Tac-Toe-Kata/src/test/java/TicTacToeBoardTest.java
+++ Tic-Tac-Toe-Kata/src/test/java/TicTacToeBoardTest.java
@@ -1,15 +1,47 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class TicTacToeBoardTest {
+
   @Test
   public void shouldReturnEmptyIfBoxIsEmpty() {
 
     Board board = new Board();
 
-    String result = board.getStatusBoxInBoard(1, 1);
+    board.setStatusBoxInBoard(1, 1, "");
 
-    assertThat("").isEqualTo(result);
+    assertThat(board.getStatusBoxInBoard(1, 1)).isEqualTo("");
+  }
+
+  @Test
+  public void shouldReturnXIfBoxIsX() {
+
+    Board board = new Board();
+
+    board.setStatusBoxInBoard(1, 1, "X");
+
+    assertThat(board.getStatusBoxInBoard(1, 1)).isEqualTo("X");
+  }
+
+  @Test
+  public void shouldReturnYIfBoxIsY() {
+
+    Board board = new Board();
+
+    board.setStatusBoxInBoard(1, 1, "Y");
+
+    assertThat(board.getStatusBoxInBoard(1, 1)).isEqualTo("Y");
+  }
+
+  @Test
+  public void shouldReturnEmptyIfBoxIsNotXOrY() {
+
+    Board board = new Board();
+
+    board.setStatusBoxInBoard(1, 1, "n");
+
+    assertThat(board.getStatusBoxInBoard(1, 1)).isEqualTo("");
   }
 }
